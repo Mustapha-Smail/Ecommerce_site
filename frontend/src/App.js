e5d21536c036ea9ee443aa401d294618717526c8
Mustapha <sml.mus.99@gmail.com>
2021-12-29 22:19:44 +0100

    Add HomeScreen + Product Card

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
+import { Header, Footer } from './components'
+import { Home } from './screens'
 
 const App = () => {
   return (
     <>
-      <h1>Welcome To ProShop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome To ProShop</h1>
+          <Home/>
+        </Container>
+      </main>
+      <Footer />      
     </>
   )
 }
