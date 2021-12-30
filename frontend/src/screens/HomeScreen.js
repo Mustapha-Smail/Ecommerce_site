05599bc4aff50029f15307c2db01b6c65944e61e
Mustapha <sml.mus.99@gmail.com>
2021-12-30 23:36:04 +0100

    setup Loader and Message (for errors) component

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
-import { Product } from '../components'
+import { Product, Message, Loader } from '../components'
 import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
@@ -19,9 +19,9 @@ const HomeScreen = () => {
         <>
             <h1>Latest Products</h1>
             {loading 
-                ? <h2>Loading...</h2> 
+                ? <Loader />
                 : error 
-                ? <h3>{error}</h3> 
+                ? <Message variant='danger'>{error}</Message> 
                 : <Row>
                     {products.map((product) => (
                         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
