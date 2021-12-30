a3b1491f9e34673006b01bf57179d9639d4b0075
Mustapha <sml.mus.99@gmail.com>
2021-12-30 16:25:58 +0100

    setup product routes

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import express from 'express'
 import cors from 'cors'
 import colors from 'colors'
 import connectDB from './config/db.js'
-import products from '../backend/data/products.js'
+import productRoutes from './routes/productRoutes.js'
 
 const app = express()
 
@@ -16,14 +16,8 @@ app.get('/', (req, res)=>{
     res.send('API is running...')
 })
 
-app.get('/api/products', (req, res)=>{
-    res.json(products)
-})
+app.use('/api/products', productRoutes)
 
-app.get('/api/product/:id', (req, res)=>{
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
 
 const PORT = process.env.PORT || 5000
 
