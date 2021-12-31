683ab02215ad88e4b8b6252b26e7396fecee2f3f
Mustapha <sml.mus.99@gmail.com>
2021-12-31 16:42:35 +0100

    setup remove items from cart functionality

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import { CART_ADD_ITEM } from '../constants/cartConstants'
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants'
 
 //getState allows to get the entire state tree (cf. store.js (reducer))
 
@@ -21,4 +21,13 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
     // We can only save strings in localStorage 
     
     localStorage.setItem('cartItem', JSON.stringify(getState().cart.cartItems))
+}
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+    dispatch({
+        type: CART_REMOVE_ITEM, 
+        payload: id, 
+    })
+
+    localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems))
 }
--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col, ListGroup, Image, Form, Button, Card } from 'react-bootstrap'
 import { Message } from '../components'
 
-import { addToCart } from '../actions/cartActions'
+import { addToCart, removeFromCart } from '../actions/cartActions'
 
 
 const CartScreen = () => {
@@ -27,7 +27,7 @@ const CartScreen = () => {
     }, [dispatch, productId, qty])
 
     const removeFromCartHandler = (id) => {
-        console.log('remove');
+        dispatch(removeFromCart(id))
     }
 
     const checkoutHandler = () => {
