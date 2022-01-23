284720515f9120e0ced457f64890de92bd8dcec4
Mustapha <sml.mus.99@gmail.com>
2022-01-23 16:17:25 +0100

    Create Order Functionnality

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -16,7 +16,7 @@ const addOrderItems = asyncHandler(async (req, res) => {
         totalPrice 
     } = req.body
 
-    if(orderItems && orderItems === 0){
+    if(orderItems && orderItems.length === 0){
         res.status(400)
         throw new Error('No order items')
     } else {
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,6 +5,7 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import { productListReducer, productDetailsReducer } from './reducers/productReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { userLoginReducer, userRegisterReducer, userDetailsReducer, userUpdateProfileReducer } from './reducers/userReducers'
+import { orderCreateReducer } from './reducers/orderReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -14,7 +15,8 @@ const reducer = combineReducers({
     userLogin: userLoginReducer, 
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
-    userUpdateProfile: userUpdateProfileReducer
+    
+    orderCreate: orderCreateReducer
 })
 
 const cartItemsfromStorage = localStorage.getItem('cartItems') 
